83d0e8b00ffd7022d60f30c9bab3677c461ddf57
Siete7jjandrades <jjandradesmarin@gmail.com>
2021-10-05 12:38:18 -0400

    firt report

---
+++ src/corel.js
@@ -0,0 +1,19 @@
+function buscarItem(lista, valor)
+{
+var ind, pos;
+for(ind=0; ind<lista.length; ind++)
+    {
+    if (lista[ind] == valor) break;
+    }
+pos = (ind < lista.length)? ind : -1; return (pos);
+}
+
+var lista1 = new Array('p2r67c');
+var cadena = prompt("Introduce el codigo");
+var cad = cadena.toLowerCase();
+var pos = buscarItem(lista1, cad);
+
+if (pos >=0) window.location.replace("video/CorelDraw.html");
+   
+    else window.location.replace("error.html");
+
